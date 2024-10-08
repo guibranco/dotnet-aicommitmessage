cb80dce2c2cfacccb9841295305d2138040e2cfd
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-10-08 15:43:09 +0100

    type - feat: extract issue number from branch names

--- Tests/AiCommitMessage.Tests/Utility/BranchNameUtilityTests.cs
+++ Tests/AiCommitMessage.Tests/Utility/BranchNameUtilityTests.cs
@@ -1,27 +1,30 @@
-using NUnit.Framework;
 using AiCommitMessage.Utility;
+using FluentAssertions;
 
-namespace AiCommitMessage.Tests.Utility
+namespace AiCommitMessage.Tests.Utility;
+
+public class BranchNameUtilityTests
 {
-    [TestFixture]
-    public class BranchNameUtilityTests
+    [Theory]
+    [InlineData("feature/123-duplicated-schedule-in-payment-lock", "123")]
+    [InlineData("bugfix/456-fix-login-bug", "456")]
+    [InlineData("feature/issue123-payment-lock-bug", "123")]
+    [InlineData("hotfix/789-fix-urgent-crash", "789")]
+    [InlineData("feature/ISSUE987-some-feature", "987")]
+    [InlineData("enhancement/ISSUE-654-enhance-performance", "654")]
+    [InlineData("release/321", "321")]
+    [InlineData("task/ISSUE-1111-complete-task", "1111")]
+    [InlineData("chore/no-issue-number", "No issue number found.")]
+    public void ExtractIssueNumber_ShouldReturnCorrectIssueNumber(
+        string branchName,
+        string expectedIssueNumber
+    )
     {
-        [TestCase("feature/123-duplicated-schedule-in-payment-lock", "123")]
-        [TestCase("bugfix/456-fix-login-bug", "456")]
-        [TestCase("feature/issue123-payment-lock-bug", "123")]
-        [TestCase("hotfix/789-fix-urgent-crash", "789")]
-        [TestCase("feature/ISSUE987-some-feature", "987")]
-        [TestCase("enhancement/ISSUE-654-enhance-performance", "654")]
-        [TestCase("release/321", "321")]
-        [TestCase("task/ISSUE-1111-complete-task", "1111")]
-        [TestCase("chore/no-issue-number", "No issue number found.")]
-        public void ExtractIssueNumber_ShouldReturnCorrectIssueNumber(string branchName, string expectedIssueNumber)
-        {
-            // Act
-            var result = BranchNameUtility.ExtractIssueNumber(branchName);
+        // Act
+        var result = BranchNameUtility.ExtractIssueNumber(branchName);
 
-            // Assert
-            Assert.AreEqual(expectedIssueNumber, result);
-        }
+        // Assert
+        result.Should().NotBeEmpty();
+        result.Should().Be(expectedIssueNumber);
     }
 }
