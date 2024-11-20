2794b55367a3d36864c5b5ca2f77008ecb5c9ea5
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-11-20 11:02:12 +0000

    Update Constants.cs

--- Src/AiCommitMessage/Utility/Constants.cs
+++ Src/AiCommitMessage/Utility/Constants.cs
@@ -9,23 +9,23 @@ public class Constants
     /// The system message.
     /// </summary>
     public const string SystemMessage = """
-        You are an assistant specialized in analyzing the quality of commits for GitHub, using the output of the branch name, the author original message (that can be empty or a single dot), and the output of GIT DIFF command.
+        You are an assistant specialized in analyzing the quality of commits for GitHub, using the output of the branch name, the author's original message (that can be empty or a single dot), and the output of the GIT DIFF command.
         Classifying them according to the following recommendations list:
 
         RECOMMENDATIONS (type - meaning):
         initial commit - commits for when the diff file is empty and there is no history in the repository (only the very beginning commits area allowed for this type).
         feat - Commits of type feat indicate that your code snippet is adding a new feature (related to MINOR in semantic versioning). Suggest this when the branch name starts with the feature or feat words.
-        fix - Commits of type fix indicate that your committed code snippet is solving a problem (bug fix) (related to PATCH in semantic versioning). Suggest this when the branch name starts with fix, hotfix, bugfix, bug.
+        fix - Commits of type fix indicate that your committed code snippet is solving a problem (bug fix) (related to PATCH in semantic versioning). Suggest this when the branch name starts with fix, hotfix, bugfix, and bug.
         docs - Commits of type docs indicate that there have been changes in the documentation, such as in your repository’s Readme or the docs directory. (Does not include code changes).
         test - Commits of type test are used when changes are made to tests, whether creating, altering, or deleting unit/integration tests under the tests directory. (Does not include code changes).
         build - Commits of type build are used when modifications are made to build files and dependencies, generally in the build, .github, and Terraform directories.
         perf - Commits of type perf are used to identify any code changes related to performance.
         style - Commits of type style indicate that there have been changes related to code formatting, semicolons, trailing spaces, lint… (Does not include code changes).
-        refactor - Commits of type refactor refer to changes due to refactoring that do not alter functionality, such as a change in the way a part of the screen is processed but maintaining the same functionality or performance improvements due to a code review.
+        refactor - Commits of type refactor refer to changes due to refactoring that do not alter functionality, such as a change in how a part of the screen is processed but maintaining the same functionality or performance improvements due to a code review.
         chore - Commits of type chore indicate updates on building tasks, admin configurations, packages, etc... Such as adding a package to a .gitignore file or updating a package dependency like NuGet, NPM, Cargo, Packagist, etc... (Does not include code changes).
         ci - Commits of type ci indicate changes related to continuous integration. This should be related to files like appveyor.yml, any .yml files under the .github/workflows directory, a config.yml at the root level, or any file inside the build directory with the .yml extension.
         raw - Commits of type raw indicate changes related to configuration files, data, features, and parameters.
-        cleanup - Commits of type cleanup are used to remove commented code, unnecessary snippets, or any other form of source code cleanup, aiming to improve its readability and maintainability.
+        cleanup - Commits of type cleanup are used to remove commented code, unnecessary snippets, or any other source code cleanup, aiming to improve its readability and maintainability.
         remove - Commits of type remove indicate the deletion of obsolete or unused files, directories, or functionalities, reducing the project’s size and complexity and keeping it more organized.
 
         OUTPUT: type - description of changes in up to 10 words in English.
